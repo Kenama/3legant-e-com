7b558b0697686fbd1817a74c4a547b9888f8bd02
hash1riya <daniildrok1@gmail.com>
2025-04-09 20:54:25 +0200

    catalog in progress

--- src/assets/js/catalog.js
+++ src/assets/js/catalog.js
@@ -11,3 +11,41 @@ function selectGrid() {
 Array.from(gridButtons.children).forEach((gridButton) => {
   gridButton.addEventListener("click", selectGrid);
 });
+
+// --------------------------------------------
+
+// data
+const products = [
+  {
+    name: "Loveseat Sofa",
+    price: 400,
+    discount: 20,
+    rating: 5,
+    imagePath: "/src/assets/img/image-loveseat-sofa.png",
+  },
+  {
+    name: "Table lamp",
+    price: 24.99,
+    discount: 30,
+    rating: 5,
+    imagePath: "/src/assets/img/image-table-lamp.png",
+  },
+  {
+    name: "Beige table lamp",
+    price: 24.99,
+    discount: 10,
+    rating: 5,
+    imagePath: "/src/assets/img/image-beige-table-lamp.png",
+  },
+  {
+    name: "Bamboo basket",
+    price: 24.99,
+    discount: 5,
+    rating: 5,
+    imagePath: "/src/assets/img/image-bamboo-basket.png",
+  },
+];
+
+// Container
+const productsContainer = document.querySelector("#products-container");
+
